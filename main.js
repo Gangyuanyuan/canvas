69133565f4a9b8b0dbfa82f5726e0d9cdb0c7536
Gangyuan <296671396@qq.com>
2019-03-19 14:10:59 +0800

    添加橡皮擦

--- main.js
+++ main.js
@@ -1,54 +1,83 @@
 var yyy = document.getElementById('xxx');
-
-WHxxx();
-window.onreset = function() {
-  WHxxx();
-}
-
 var context = yyy.getContext('2d');
 
-painting = false;
-var lastPoint = {x:undefined,y:undefined}
-yyy.onmousedown = function(aaa) {
-  painting = true;
-  var x = aaa.clientX;
-  var y = aaa.clientY;
-  lastPoint = {"x":x,"y":y};
-  drawCircle(x,y,1);
+autoSetCanvasSize(yyy);
+
+listonToMouse(yyy);
+
+var eraserEnabled = false;
+eraser.onclick = function(){
+  	eraserEnabled = true;
+  	actions.className = 'actions x';
 }
-yyy.onmousemove = function(aaa) {
-  if(painting){
-    var x = aaa.clientX;
-    var y = aaa.clientY;
-    var newPoint = {"x":x,"y":y};
-    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
-    lastPoint = newPoint;
-  }
-}
-yyy.onmouseup = function(aaa) {
-  painting = false;
+brush.onclick = function(){
+  	eraserEnabled = false;
+  	 actions.className = 'actions';
 }
 
-function WHxxx() {
-  var pageWidth = document.documentElement.clientWidth;  // 获取用户屏幕宽度
-  var pageHeight = document.documentElement.clientHeight;// 获取用户屏幕高度
-  yyy.width = pageWidth;
-  yyy.height = pageHeight;
+
+/******************/
+
+function autoSetCanvasSize(canvas){
+	setCanvasSize();
+  	window.onresize = function(){
+    	setCanvasSize();
+  	}
+	function setCanvasSize(){
+    	var pageWidth = document.documentElement.clientWidth;    // 获取用户屏幕宽度
+    	var pageHeight = document.documentElement.clientHeight;  // 获取用户屏幕高度
+    	canvas.width = pageWidth;
+    	canvas.height = pageHeight;
+  	}
 }
 
-function drawCircle(x,y,radius) {
-  context.beginPath();
-  context.fillStyle = 'black';
-  context.arc(x,y,radius,0,Math.PI*2);
-  context.fill();
+function drawCircle(x,y,radius){
+  	context.beginPath();
+  	context.fillStyle = 'black';
+  	context.arc(x,y,radius,0,Math.PI*2);
+  	context.fill();
 }
 
-function drawLine(x1,y1,x2,y2) {
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.moveTo(x1,y1);   // 起点
-  context.lineWidth = 5;
-  context.lineTo(x2,y2);   // 终点
-  context.closePath();
-  context.stroke();
+function drawLine(x1,y1,x2,y2){
+  	context.beginPath();
+  	context.strokeStyle = 'black';
+  	context.moveTo(x1,y1);   // 起点
+  	context.lineWidth = 5;
+  	context.lineTo(x2,y2);   // 终点
+  	context.closePath();
+  	context.stroke();
+}
+
+function listonToMouse(canvas){
+	var using = false;
+	var lastPoint = {
+		x:undefined,
+		y:undefined
+	}
+	canvas.onmousedown = function(aaa){
+		var x = aaa.clientX;
+		var y = aaa.clientY;
+		using = true;
+		if(eraserEnabled){
+    		context.clearRect(x-5,y-5,10,10); // 鼠标默认位置为长方形左上角
+		}else{
+			lastPoint = {"x":x,"y":y};
+			drawCircle(x,y,1);
+		}
+	}
+	canvas.onmousemove = function(aaa){
+		var x = aaa.clientX;
+		var y = aaa.clientY;
+		if(!using) {return}
+		if(eraserEnabled){
+			context.clearRect(x-5,y-5,10,10);
+		}else{
+			var newPoint = {"x":x,"y":y};
+			drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
+			lastPoint = newPoint;
+		}
+	}
+	canvas.onmouseup = function(aaa){
+		using = false;
+	}
 }
