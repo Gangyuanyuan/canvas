ecd49836cd4bfed4443d3a08dfbd9c42122dc62d
Gangyuan <296671396@qq.com>
2019-03-20 22:33:29 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var context = yyy.getContext('2d');
 
 autoSetCanvasSize(yyy);
 
-listonToMouse(yyy);
+listonToUser(yyy);
 
 var eraserEnabled = false;
 eraser.onclick = function(){
@@ -48,36 +48,72 @@ function drawLine(x1,y1,x2,y2){
   	context.stroke();
 }
 
-function listonToMouse(canvas){
+function listonToUser(canvas){
 	var using = false;
 	var lastPoint = {
 		x:undefined,
 		y:undefined
 	}
-	canvas.onmousedown = function(aaa){
-		var x = aaa.clientX;
-		var y = aaa.clientY;
-		using = true;
-		if(eraserEnabled){
+	// 特性检测
+	if(document.body.ontouchstart !== undefined){
+		// 触屏设备
+		canvas.ontouchstart = function(aaa){
+			console.log('触摸')
+			var x = aaa.touches[0].clientX;
+			var y = aaa.touches[0].clientY;
+			using = true;
+			if(eraserEnabled){
     		context.clearRect(x-5,y-5,10,10); // 鼠标默认位置为长方形左上角
-		}else{
-			lastPoint = {"x":x,"y":y};
-			drawCircle(x,y,1);
+    		}else{
+    		lastPoint = {"x":x,"y":y};
+    		drawCircle(x,y,1);
+    		}
 		}
-	}
-	canvas.onmousemove = function(aaa){
-		var x = aaa.clientX;
-		var y = aaa.clientY;
-		if(!using) {return}
-		if(eraserEnabled){
-			context.clearRect(x-5,y-5,10,10);
-		}else{
-			var newPoint = {"x":x,"y":y};
-			drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
-			lastPoint = newPoint;
+		canvas.ontouchmove = function(aaa){
+			console.log('移动')
+			var x = aaa.touches[0].clientX;
+    		var y = aaa.touches[0].clientY;
+    		if(!using) {return}
+    		if(eraserEnabled){
+    			context.clearRect(x-5,y-5,10,10);
+    		}else{
+    			var newPoint = {"x":x,"y":y};
+    			drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
+    			lastPoint = newPoint;
+    		}
 		}
-	}
-	canvas.onmouseup = function(aaa){
-		using = false;
-	}
+		canvas.ontouchend = function(aaa){
+			console.log('离开')
+			using = false;
+		}
+	}else{
+		// 非触屏设备
+		canvas.onmousedown = function(aaa){
+			var x = aaa.clientX;
+			var y = aaa.clientY;
+			using = true;
+			if(eraserEnabled){
+    		context.clearRect(x-5,y-5,10,10); // 鼠标默认位置为长方形左上角
+    		}else{
+    		lastPoint = {"x":x,"y":y};
+    		drawCircle(x,y,1);
+    		}
+    	}
+    	canvas.onmousemove = function(aaa){
+    		var x = aaa.clientX;
+    		var y = aaa.clientY;
+    		if(!using) {return}
+    		if(eraserEnabled){
+    			context.clearRect(x-5,y-5,10,10);
+    		}else{
+    			var newPoint = {"x":x,"y":y};
+    			drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
+    			lastPoint = newPoint;
+    		}
+    	}
+    	canvas.onmouseup = function(aaa){
+    		using = false;
+    	}
+    }	
 }
+
